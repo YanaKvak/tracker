2eb54339a2d8b6af630ab188e690f54c868be1b6
YanaKvak <anaKvak2@gmail.com>
2025-04-18 20:23:48 +0300

    бек+не рабочий фронт

--- front/src/api/projectApi.js
+++ front/src/api/projectApi.js
@@ -1,7 +1,7 @@
 // src/api/projectApi.js (создайте новый файл)
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000'; // Или ваш базовый URL
+const API_BASE_URL = 'http://localhost:8080'
 
 export const fetchProjects = async () => {
   try {
