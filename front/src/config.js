d4796d37fbbc649c2de2fbb9ea2e638064341362
YanaKvak <anaKvak2@gmail.com>
2025-04-28 22:45:37 +0300

    слияние веток фронт + календарь 1 на всех пользователей необходимо поправить чтобы у всех был разный

--- front/src/config.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
-export const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL || 'http://localhost:3000';
